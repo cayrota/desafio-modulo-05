5635e8cd503e59518a1c73b264197266706b7a96
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2021-07-31 15:33:46 -0300

    subindo o back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const login = require('./controladores/login');
+const cadastro = require('./controladores/usuarios');
+const verificaLogin = require('./filtros/verificaLogin');
+const produtos = require('./controladores/produtos');
+
+
+const rotas = express();
+
+//rotas de usuarios
+rotas.post('/cadastro', cadastro);
+rotas.post('/login', login);
+
+// rotas produtos
+rotas.get('/produtos',verificaLogin, produtos.listarProdutos);
+rotas.get('/produtos/:id',verificaLogin, produtos.consultarProdutoId);
+rotas.post('/produtos',verificaLogin, produtos.cadastrarProduto);
+rotas.put('/produtos/:id',verificaLogin, produtos.alterarProduto);
+rotas.delete('/produtos/:id',verificaLogin, produtos.removerProduto);
+rotas.post('/produtos/:id/ativar',verificaLogin, produtos.ativarProduto);
+rotas.post('/produtos/:id/desativar',verificaLogin, produtos.desativarProduto);
+
+module.exports = rotas;
