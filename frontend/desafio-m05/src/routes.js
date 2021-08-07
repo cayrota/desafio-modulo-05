18c15e2640351a392c726202c10cb879d8c89ebe
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2021-08-07 19:23:52 -0300

    Merge branch 'main' of https://github.com/cayrota/desafio-modulo-05

--- frontend/desafio-m05/src/routes.js
+++ frontend/desafio-m05/src/routes.js
@@ -1,16 +1,38 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./pages/Login/index";
-import Cadastro from "./pages/Cadastro/index";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import React from 'react';
+
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import Produtos from './pages/Produtos';
+
+import { AuthProvider } from './contexts/AuthContext';
+import useAuth from './hooks/useAuth';
+
+function RotasProtegidas(props) {
+  const { token } = useAuth();
+  return (
+    <Route render={() => (token ? props.children : <Redirect to='/' />)} />
+  );
+}
 
 function Routes() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/cadastro" exact component={Cadastro} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Login} />
+          <Route path='/cadastro' component={Cadastro} />
+          <RotasProtegidas>
+            <Route path='/produtos' component={Produtos} />
+          </RotasProtegidas>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
